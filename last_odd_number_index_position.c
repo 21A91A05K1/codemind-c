04802ce369c6ae9400c79c4bab8a6a4a294949a3
21A91A05K1 <97425986+21A91A05K1@users.noreply.github.com>
2022-04-15 20:38:38 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,max=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n-1;i++)     {         if(arr[i]%2!=0)         {             if(arr[i]>max)             {                 max=arr[i];             }         }     }     printf("%d",i); }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+    }
+    printf("%d",i);
+}
