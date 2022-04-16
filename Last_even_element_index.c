3b9bb9b70c35eb99f81b58c10e1193a0787053e7
21A91A05K1 <97425986+21A91A05K1@users.noreply.github.com>
2022-04-16 09:03:13 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,max=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             if(arr[i]>max)             {                 max=i;             }         }     }     printf("%d",max); }

---
+++ Last_even_element_index.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,max=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            if(arr[i]>max)
+            {
+                max=i;
+            }
+        }
+    }
+    printf("%d",max);
+}
