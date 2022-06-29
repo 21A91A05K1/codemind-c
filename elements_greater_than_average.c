27850e29a0c915a9166dce8f823c16dc5ca9e455
21A91A05K1 <97425986+21A91A05K1@users.noreply.github.com>
2022-06-29 19:07:48 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,ave,sum=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];     }     ave=sum/n;     for(i=0;i<n;i++)     {         if(arr[i]>=ave)         {             c++;         }     }     printf("%d",c); }

---
+++ elements_greater_than_average.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,ave,sum=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+    }
+    ave=sum/n;
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>=ave)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+}
